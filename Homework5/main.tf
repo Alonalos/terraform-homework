5c1747a7a664fcccdf350033e5af7cb187ef6de9
alonalos <alonalos8@gmail.com>
2024-10-31 00:00:04 +0000

    hw5

---
+++ Homework5/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name = aws_key_pair.deployer2.key_name
+  user_data = file("apache.sh")
+
+}
+
---
+++ Homework5/provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_key_pair" "deployer2" {
+  key_name   = "deployer-key2"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
