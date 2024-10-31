7538972e955de5a085aa3a377a63cbeff637badc
Alona_a <58535834+Alonalos@users.noreply.github.com>
2024-10-30 20:04:28 -0400

    Update sg.tf

--- Homework4/sg.tf
+++ Homework4/sg.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "allow_tls" {
 
   ingress {
     description = "TLS from VPC"
-    from_port   = var.ports[0]
+    from_port   = var.ports[2]
     to_port     = var.ports[2]
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
